a1d9acd7f932db6b5543d1acd1efe048a8180904
nithinmanoj10 <64126131+nithinmanoj10@users.noreply.github.com>
2022-07-06 15:38:49 +0530

    :art: Improved Modularity and Error Handling
    + struct Arguments with fields query and file_name
    + new() constructor for the struct Argument
    + run() function that extracts the program logic henceforth

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,45 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
-    println!("GREP CLI Tool in Rust")
+    // Accepting user inputed arguments from the command line
+    let arg_list: Vec<String> = env::args().collect();
+    let arguments = Arguments::new(&arg_list).unwrap_or_else(|err| {
+        println!("Error: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(arguments) {
+        println!("Error: {}", e);
+        process::exit(1);
+    }
+}
+
+struct Arguments {
+    query: String,
+    file_name: String,
+}
+
+impl Arguments {
+    fn new(args: &[String]) -> Result<Arguments, &'static str> {
+        if args.len() != 3 {
+            return Err(
+                "Command doesn't have enough arguments\nExpected: cargo run <query> <file_name>\n",
+            );
+        }
+
+        Ok(Arguments {
+            query: args[1].clone(),
+            file_name: args[2].clone(),
+        })
+    }
+}
+
+fn run(arguments: Arguments) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(arguments.file_name)?;
+    println!("{}", contents);
+
+    Ok(())
 }
